1695a6fdfe59a586c4fed73f40d218f0bb34ebf9
HATYBANDARA <88592426+HATYBANDARA@users.noreply.github.com>
2021-12-09 09:59:15 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -110,23 +110,92 @@ async def callback_query(client: Client, query: CallbackQuery):
                 InlineKeyboardButton('👥 GROUP', callback_data="GROUP_CALLBACK"),
                 InlineKeyboardButton('📣 CHANNEL', url='https://t.me/education_bots_project_sl'),
                 InlineKeyboardButton('👨‍💻 CREATOR', url='https://t.me/Video_chat_streamer')
-            ],
-            [
-                InlineKeyboardButton('➕ CREATE YOUR BOT ➕', callback_data="TUTORIAL_CALLBACK")
-            ]
+            ],           
         ]
-        reply_markup = InlineKeyboardMarkup(TELETIPS_START_BUTTONS)
-        try:
-            await query.edit_message_text(
-                START_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass    
+        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="demo.css">
+    <title>Exam Timer</title>
+    
+</head>
+<body>
+
+  <header>
+    <h1>EXAMINATION IS STARTING SOON</h1>
+    <p>Hope say you dey prepared &#128516; </p>
+  </header>
+
+    <div class="grid-container">
+        <div  class="digit">
+          <h1>Days</h1>
+          <h1 id="demo1"></h1>
+        </div>
+
+        <div  class="digit">
+          <h1>Hours</h1>
+          <h1 id="demo2"></h1>
+        </div>
+
+        <div  class="digit">
+          <h1>Minutes</h1>
+          <h1 id="demo3"></h1>
+        </div>
+      
+        <div class="digit">
+          <h1>Seconds</h1>
+          <h1 id="demo4"></h1>
+        </div>
+    </div>
+
+    <section class="section-6">
+      <p class="text-center">
+          All Right Reserved &copy; 2021 Coded by <a href="https://github.com/SammieKei40" target="">Sammie Kei &#128540; &#128513;</a>.
+      </p>
+  </section>
 
 
 
-print("Countdown Timer is alive!")
-bot.run()
+    <script>
+        var countDownDate = new Date("August 30, 2021 00:00:00").getTime();
+    
+        var x = setInterval(function(){
+          var now = new Date().getTime();
+          
+    
+          var distance = countDownDate - now;
+    
+          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    
+          var hours = Math.floor((distance % (1000  * 60 * 60 * 24)) / (1000 * 60 * 60 ));
+    
+          var minutes = Math.floor((distance % (1000  * 60 * 60 )) / (1000 * 60));
+    
+          var seconds = Math.floor((distance % (1000  * 60)) / (1000 ));
+    
+          document.getElementById("demo1").innerHTML = days 
+          document.getElementById("demo2").innerHTML = hours 
+          document.getElementById("demo3").innerHTML = minutes
+          document.getElementById("demo4").innerHTML = seconds
+        })
+    
+        if(distance === 0){
+          clearInterval(x);
+    
+          document.getElementById("demo1").innerHTML =  "EXAM DON START!!! GOODLUCK!!!";
+          document.getElementById("demo2").innerHTML = "EXAM DON START!!! GOODLUCK!!!"; 
+          document.getElementById("demo3").innerHTML = "EXAM DON START!!! GOODLUCK!!!";
+          document.getElementById("demo4").innerHTML = "EXAM DON START!!! GOODLUCK!!!";
+        }
+      </script>
+</body>
 
-#Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
+</html>
+
+
+
+
+
