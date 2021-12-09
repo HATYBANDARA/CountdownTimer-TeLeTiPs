5b4cbbf4389ebf4f59dbc76093e24c99674962c2
HATYBANDARA <88592426+HATYBANDARA@users.noreply.github.com>
2021-12-09 10:05:55 +0530

    Calculate-Edu

--- teletips_set.py
+++ teletips_set.py
@@ -27,8 +27,8 @@ TELETIPS_MAIN_MENU_BUTTONS = [
             ],
             [
                 InlineKeyboardButton('👥 GROUP', callback_data="GROUP_CALLBACK"),
-                InlineKeyboardButton('📣 CHANNEL', url='https://t.me/education_bots_project_sl'),
-                InlineKeyboardButton('👨‍💻 CREATOR', url='https://t.me/Video_chat_streamer')
+                InlineKeyboardButton('📣 CHANNEL', url='https://t.me/teletipsofficialchannel'),
+                InlineKeyboardButton('👨‍💻 CREATOR', url='https://t.me/thakshakar')
             ],
             [
                 InlineKeyboardButton('➕ CREATE YOUR BOT ➕', callback_data="TUTORIAL_CALLBACK")
@@ -68,7 +68,7 @@ async def callback_query(client: Client, query: CallbackQuery):
                 InlineKeyboardButton("🇱🇰 Anytime Any Qs", url="https://t.me/AnytimeAnyQs")
             ],
             [
-                InlineKeyboardButton("🌎Edu_bot", url="https://t.me/education_bots_project_sl")
+                InlineKeyboardButton("🌎 TeLe TiPs", url="https://t.me/teletipsofficialontopicchat")
             ],
             [
                 InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK"),
@@ -108,94 +108,103 @@ async def callback_query(client: Client, query: CallbackQuery):
             ],
             [
                 InlineKeyboardButton('👥 GROUP', callback_data="GROUP_CALLBACK"),
-                InlineKeyboardButton('📣 CHANNEL', url='https://t.me/education_bots_project_sl'),
-                InlineKeyboardButton('👨‍💻 CREATOR', url='https://t.me/Video_chat_streamer')
-            ],           
+                InlineKeyboardButton('📣 CHANNEL', url='https://t.me/teletipsofficialchannel'),
+                InlineKeyboardButton('👨‍💻 CREATOR', url='https://t.me/thakshakar')
+            ],
+            [
+                InlineKeyboardButton('➕ CREATE YOUR BOT ➕', callback_data="TUTORIAL_CALLBACK")
+            ]
         ]
-        
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="demo.css">
-    <title>Exam Timer</title>
-    
-</head>
-<body>
+        reply_markup = InlineKeyboardMarkup(TELETIPS_START_BUTTONS)
+        try:
+            await query.edit_message_text(
+                START_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass    
 
-  <header>
-    <h1>EXAMINATION IS STARTING SOON</h1>
-    <p>Hope say you dey prepared &#128516; </p>
-  </header>
+@bot.on_message(filters.command('set'))
+async def set_timer(client, message):
+    global stoptimer
+    try:
+        if message.chat.id>0:
+            return await message.reply('⛔️ Try this command in a **group chat**.')
+        elif not (await client.get_chat_member(message.chat.id,message.from_user.id)).can_manage_chat:
+            return await message.reply('👮🏻‍♂️ Sorry, **only admins** can execute this command.')    
+        elif len(message.command)<3:
+            return await message.reply('❌ **Incorrect format.**\n\n✅ Format should be like,\n<code> /set seconds "event"</code>\n\n**Example**:\n <code>/set 86400 "TIME LEFT UNTIL NEW YEAR"</code>')    
+        else:
+            user_input_time = int(message.command[1])
+            user_input_event = str(message.command[2])
+            get_user_input_time = await bot.send_message(message.chat.id, user_input_time)
+            await get_user_input_time.pin()
+            if stoptimer: stoptimer = False
+            if 0<user_input_time<=10:
+                while user_input_time and not stoptimer:
+                    s=user_input_time%60
+                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, s)
+                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
+                    await asyncio.sleep(1)
+                    user_input_time -=1
+                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
+            elif 10<user_input_time<60:
+                while user_input_time>0 and not stoptimer:
+                    s=user_input_time%60
+                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, s)
+                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
+                    await asyncio.sleep(3)
+                    user_input_time -=3
+                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
+            elif 60<=user_input_time<3600:
+                while user_input_time>0 and not stoptimer:
+                    m=user_input_time%3600//60
+                    s=user_input_time%60
+                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**m** : {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, m, s)
+                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
+                    await asyncio.sleep(3)
+                    user_input_time -=3
+                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
+            elif 3600<=user_input_time<86400:
+                while user_input_time>0 and not stoptimer:
+                    h=user_input_time%(3600*24)//3600
+                    m=user_input_time%3600//60
+                    s=user_input_time%60
+                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**h** : {:02d}**m** : {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, h, m, s)
+                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
+                    await asyncio.sleep(7)
+                    user_input_time -=7
+                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
+            elif user_input_time>=86400:
+                while user_input_time>0 and not stoptimer:
+                    d=user_input_time//(3600*24)
+                    h=user_input_time%(3600*24)//3600
+                    m=user_input_time%3600//60
+                    s=user_input_time%60
+                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**d** : {:02d}**h** : {:02d}**m** : {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, d, h, m, s)
+                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
+                    await asyncio.sleep(9)
+                    user_input_time -=9
+                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
+            else:
+                await get_user_input_time.edit(f"🤷🏻‍♂️ I can't countdown from {user_input_time}")
+                await get_user_input_time.unpin()
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
 
-    <div class="grid-container">
-        <div  class="digit">
-          <h1>Days</h1>
-          <h1 id="demo1"></h1>
-        </div>
+@bot.on_message(filters.command('stopc'))
+async def stop_timer(Client, message):
+    global stoptimer
+    try:
+        if (await bot.get_chat_member(message.chat.id,message.from_user.id)).can_manage_chat:
+            stoptimer = True
+            await message.reply('🛑 Countdown stopped.')
+        else:
+            await message.reply('👮🏻‍♂️ Sorry, **only admins** can execute this command.')
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
 
-        <div  class="digit">
-          <h1>Hours</h1>
-          <h1 id="demo2"></h1>
-        </div>
+print("Countdown Timer is alive!")
+bot.run()
 
-        <div  class="digit">
-          <h1>Minutes</h1>
-          <h1 id="demo3"></h1>
-        </div>
-      
-        <div class="digit">
-          <h1>Seconds</h1>
-          <h1 id="demo4"></h1>
-        </div>
-    </div>
-
-    <section class="section-6">
-      <p class="text-center">
-          All Right Reserved &copy; 2021 Coded by <a href="https://github.com/SammieKei40" target="">Sammie Kei &#128540; &#128513;</a>.
-      </p>
-  </section>
-
-
-
-    <script>
-        var countDownDate = new Date("August 30, 2021 00:00:00").getTime();
-    
-        var x = setInterval(function(){
-          var now = new Date().getTime();
-          
-    
-          var distance = countDownDate - now;
-    
-          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    
-          var hours = Math.floor((distance % (1000  * 60 * 60 * 24)) / (1000 * 60 * 60 ));
-    
-          var minutes = Math.floor((distance % (1000  * 60 * 60 )) / (1000 * 60));
-    
-          var seconds = Math.floor((distance % (1000  * 60)) / (1000 ));
-    
-          document.getElementById("demo1").innerHTML = days 
-          document.getElementById("demo2").innerHTML = hours 
-          document.getElementById("demo3").innerHTML = minutes
-          document.getElementById("demo4").innerHTML = seconds
-        })
-    
-        if(distance === 0){
-          clearInterval(x);
-    
-          document.getElementById("demo1").innerHTML =  "EXAM DON START!!! GOODLUCK!!!";
-          document.getElementById("demo2").innerHTML = "EXAM DON START!!! GOODLUCK!!!"; 
-          document.getElementById("demo3").innerHTML = "EXAM DON START!!! GOODLUCK!!!";
-          document.getElementById("demo4").innerHTML = "EXAM DON START!!! GOODLUCK!!!";
-        }
-      </script>
-</body>
-
-</html>
-
-
-
-
-
+#Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
