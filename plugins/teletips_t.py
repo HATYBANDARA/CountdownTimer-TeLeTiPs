a96e941d971f0e1530098d5fe4da3f1f7bf778c4
HATYBANDARA <88592426+HATYBANDARA@users.noreply.github.com>
2021-12-09 08:31:55 +0530

    Update teletips_t.py

--- plugins/teletips_t.py
+++ plugins/teletips_t.py
@@ -1,6 +1,6 @@
-#Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
+#Copyright ©️ 2021 Edu Bot . All Rights Reserved
 #You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits - [Countdown Timer Telegram bot by TeLe TiPs] (https://github.com/teletips/CountdownTimer-TeLeTiPs)
+# ##Credits - [TimeCalculate Telegram bot by Edu_bot] (https://github.com/HATYBANDARA/TimeCalculate-Edu_bot)
 
 # Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/CountdownTimer-TeLeTiPs/blob/main/LICENSE
                        
